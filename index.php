792c4d3029093ca10b2530eac0d6f0395dad2f8a
360deg <osa.9735@yahoo.com>
2018-11-06 16:23:11 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vlad Pecherytsya | Blog</title>
+    <script src=kek.js></script>
+    <link rel="shortcut icon" type="image/x-icon" href="icons/favicon.ico" />
+    <link rel=stylesheet type="text/css" href="style.css">
+</head>
+<body class="pisya">
+<!-- The overlay -->
+<div id="myNav" class="overlay">
+
+    <!-- Button to close the overlay navigation -->
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+    <!-- Overlay content -->
+    <div class="overlay-content">
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+
+</div>
+<div class="grad1">
+    <div class="card">
+        <img src="img_avatar.png" alt="Avatar" style="width:100%">
+        <div class="container">
+            <h4><b>John Doe</b></h4>
+            <p>Architect & Engineer</p>
+        </div>
+    </div>
+</div>
+<!-- Use any element to open/show the overlay navigation menu -->
+<button onclick="openNav()">open</button>
+<button onclick="pisya()">pisya</button>
+
+</body>
+</html>
